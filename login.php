5b3420e187890823d3657459e7ae240e34892750
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-05-26 21:54:32 -0500

    iniciando login

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <p>usuario</p>
+        <input type="text" name="usuario" placeholder="ingrese ususario">
+        <p>contraceña</p>
+        <input type="password" name="contraceña" placeholder="ingrese contraceña">
+        <button>
+            ingresar
+        </button>
+    </form>
+</body>
+
+</html>
