8ddb8046356d5114d2f5a3cffd8a6a8b6f9915eb
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-06-12 17:27:15 -0500

    avance

--- login.php
+++ login.php
@@ -6,18 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+
 </head>
 
 <body>
+    <h1>Login</h1>
     <form action="">
         <p>usuario</p>
-        <input type="text" name="usuario" placeholder="ingrese ususario">
+        <input type="text" name="Usuario" placeholder="ingrese ususario">
         <p>contraceña</p>
-        <input type="password" name="contraceña" placeholder="ingrese contraceña">
+        <input type="password" name="Contraceña" placeholder="ingrese contraceña">
+        <br>
+        <br>
         <button>
             ingresar
         </button>
+        <button>
+        <a href="ir a registro">ir a registro</a></button>
     </form>
 </body>
+
+
+</html>
 
 </html>
